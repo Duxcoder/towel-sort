e47f0d0e90132ed9dd5c4ab9eab93141b33ae0a7
Duxcoder <iinegve_25@mail.ru>
2023-01-05 19:06:36 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    const towelSortRun = () => {
+       return matrix.reduce((arr, item, i) => {
+            if (i % 2) return [...arr, ...item.reverse()]
+            return [...arr, ...item];
+        }, [])
+    }
+    return matrix ? towelSortRun() : []
 }
